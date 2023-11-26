c654d03a22781f7a70eae1516874f5b0786f2a75
Franco Caraffa <francocaraffa2005@gmail.com>
2023-11-26 00:19:34 -0300

    Cambios de diseño

--- app/page.js
+++ app/page.js
@@ -5,17 +5,17 @@ export default function Home() {
   return (
   <>
     <header className='shadow-md'>
-      <ul className='flex justify-center gap-10 p-10 text-[#FDF0D5]'>
+      <ul className='flex justify-end gap-10 p-10 text-[#FDF0D5]'>
         <li>Inicio</li>
         <li>Sobre Mi</li>
         <li>Contacto</li>
       </ul>
     </header>
     <main>
-      <section className='mt-20 flex justify-center'>
+      <section className='mt-20 flex justify-center '>
         <div className='flex-col flex'>
-          <h1 className='text-3xl font-bold text-[#FDF0D5] mb-10'><span className='text-xl'>¡Hola!</span><br/>Soy Franco Caraffa</h1>
-          <h2 className='font-black text-5xl text-[#669BBC] underline'>Frontend developer</h2>
+          <h1 className='text-6xl font-bold text-[#FDF0D5] mb-10'><span className='text-[16px] text-[#FCDA95]'>¡Hola! Mi nombre es,</span><br/>Franco Caraffa.</h1>
+          <h2 className='font-black text-7xl text-[#669BBC] underline'>Frontend developer</h2>
           <div className='flex gap-5 mt-7'>
             <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35" fill="none" viewBox="0 0 76 76">
               <path fill="#FDF0D5" fill-rule="evenodd" d="M38 0c20.987 0 38 17.442 38 38.961 0 17.21-10.876 31.81-25.965 36.967-1.927.383-2.611-.833-2.611-1.87 0-1.285.046-5.48.046-10.694 0-3.04 2.257-7.858 5.062-9.029 5.44-2.27 9.712-7.035 9.712-17.407 0-1.947-.308-3.73-.876-5.352-1.21-3.457-2.316-5.703-2.27-9.366.036-2.89-2.891-6.378-5.594-5.354-.333.126-.685.271-1.057.439-3.293 1.477-6.931 2.17-10.518 1.772A37.188 37.188 0 0 0 38 18.84a37.5 37.5 0 0 0-3.923.226c-3.586.399-7.224-.293-10.517-1.77-.37-.165-.72-.31-1.05-.435-2.716-1.03-5.652 2.471-5.609 5.374.054 3.652-1.047 5.878-2.262 9.321-.573 1.628-.887 3.417-.887 5.372 0 12.196 5.925 16.66 12.696 18.422 1.764.46 2.94 2.874 2.48 4.637-.184.7-.669 1.6-1.352 1.837-2.535.88-6.933 1.446-9.856-3.451-.084-.142-.163-.288-.25-.428-.462-.746-2.204-3.242-5.122-3.736a3.981 3.981 0 0 0-1.16-.033c-.668.084-1.587.358-.905 1.253.66.866 1.74 1.31 2.52 2.071.825.804 1.904 2.176 2.804 4.398.082.204.154.413.243.613.39.883 1.813 3.44 5.493 4.405 3.607.947 7.24 3.412 7.24 7.141 0 1.03-.699 2.235-2.595 1.874C10.887 70.782 0 56.175 0 38.961 0 17.441 17.016 0 38 0Z" clip-rule="evenodd"/>
@@ -29,19 +29,21 @@ export default function Home() {
             <button className='bg-[#FDF0D5] p-2 rounded w-[150px] ml-5'>Descargar CV</button>
           </div>
         </div>
-        <Image className='rounded-[50%] ml-5' src={foto} width={230} height={50}/>
+        
       </section>
-      <div className='bg-[#1D1C1C] mt-20 h-[1000px]'>
-        <section>
-          <h2>Acerca de mí</h2>
-          <p>ASD</p>
-        </section>
-        <section>
-          <h2>Tecnologias</h2>
-        </section>
-        <section>Proyectos</section>
-        <section>Contactame</section>
-        <footer>Made by Franco Caraffa</footer>
+      <div className=' mt-32 h-[1000px]'>
+        <div className='max-w-[1300px] m-auto p-10'>
+          <section className='shadow-2xl'>
+            <h2 className='font-bold text-4xl text-[#FCDA95] mb-2'>&lt;Acerca de mi/&gt;</h2>
+            <p className='text-[#FDF0D5] p-7 text-[18px]'>¡Hola! Soy un apasionado desarrollador frontend con una dedicación inquebrantable hacia la creación de experiencias web cautivadoras y funcionales. Mi viaje en el mundo del desarrollo web comenzó con la emoción de convertir ideas creativas en realidades digitales, y desde entonces, he estado inmerso en el emocionante universo del frontend.</p>
+          </section>
+          <section>
+            <h2 className='font-bold text-4xl text-[#FCDA95] mt-32'>Tecnologias</h2>
+          </section>
+          <section>Proyectos</section>
+          <section>Contactame</section>
+          <footer>Made by Franco Caraffa</footer>
+        </div>
       </div>
     </main>
   </>
