a9c5f0f277e7b3661040b8c71f663a11759d5084
Franco Caraffa <francocaraffa2005@gmail.com>
2023-11-27 20:52:47 -0300

    Flecha hacia abajo y mejora del hero

--- app/components/BarraResumen.jsx
+++ app/components/BarraResumen.jsx
@@ -1,10 +1,10 @@
 export function BarraResumen(){
     return(
-      <section className='border-[#355d75] border rounded mt-48 p-7  m-auto w-[100%]'>
+      <section className='border-[#355d75] border rounded  p-8 m-auto w-[100%] shadow-2xl'>
         <ul className='flex text-[#FDF0D5] justify-between text-center'>
-          <li className='text-3xl font-black'>+2<br/><span className='text-xl font-normal'>Años de experiencia</span></li>
-          <li className='text-3xl font-black'>+10<br/><span className='text-xl font-normal'>Proyectos</span></li>
-          <li className='text-3xl font-black'>+5<br/><span className='text-xl font-normal'>Clientes</span></li>
+          <li className='text-3xl font-black w-[20%]'>+10<br/><span className='text-xl font-normal'>Proyectos</span></li>
+          <li className='text-3xl font-black  w-[20%]'>+2<br/><span className='text-xl font-normal'>Años de experiencia</span></li>
+          <li className='text-3xl font-black  w-[20%]'>+5<br/><span className='text-xl font-normal'>Clientes</span></li>
         </ul>
       </section>
     )
--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,9 +1,12 @@
 import { MyComponent } from "./Componente"
 import { TikTokIcon, GitHubIcon, LinkedinIcon } from "./Icons"
+import { FaAngleDoubleDown } from "react-icons/fa";
+
+
 
 export function Hero(){
     return(
-      <section className='mt-32 flex justify-start '>
+      <section className='flex flex-col justify-center min-h-screen'>
         <div className='flex-col flex'>
           <h1 className='text-6xl font-bold text-[#FDF0D5] mb-10'><span className='text-[16px] text-[#FCDA95]'>¡Hola! Mi nombre es,</span><br/>Franco Caraffa.</h1>
           <MyComponent/>
@@ -13,6 +16,9 @@ export function Hero(){
             <LinkedinIcon/>
             <button className='bg-[#FDF0D5] p-2 rounded w-[150px] ml-5'>Descargar CV</button>
           </div>
+        </div>
+        <div className="absolute bottom-24 left-[49.2%] animate-bounce">
+          <FaAngleDoubleDown color="#FDF0D5"  size={35}/>
         </div>
       </section>
     )
