c818adbaa82fc6608a3d6efb23e0befc97d15924
Franco Caraffa <francocaraffa2005@gmail.com>
2023-12-05 20:40:56 -0300

    Detalles de diseño

--- app/components/Cards.jsx
+++ app/components/Cards.jsx
@@ -6,6 +6,7 @@ import { useEffect } from "react"
 import AOS from "aos"
 import 'aos/dist/aos.css'
 import { GitHubIconDark } from './Icons';
+import { FaExternalLinkAlt } from "react-icons/fa";
 
 
 export function Proyectos({children}){
@@ -16,8 +17,8 @@ export function Proyectos({children}){
 
     return(
     <div data-aos="zoom-in">
-        <h1 className='font-black text-5xl text-[#669BBC] mt-32 mb-10 underline'>Proyectos</h1>
-        <div className='grid grid-cols-3 justify-items-center gap-4'>
+        <h1 className='font-black text-5xl text-[#669BBC] mt-32 mb-10'>PROYECTOS</h1>
+        <div className='grid grid-cols-3 justify-items-center gap-5'>
           {children}
         </div>
     </div>
@@ -26,13 +27,17 @@ export function Proyectos({children}){
 
 export function Cards({img, url, titulo, desc, github}) {
   return (
-    <div className='w-[100%] border-2 border-[#293e4b] shadow-2xl rounded-2xl overflow-hidden bg-[#669BBC] flex flex-col h-[400px] transition-all hover:scale-105'>
+    <div className='w-[100%] border-2 border-[#293e4b] shadow-2xl rounded-2xl overflow-hidden bg-[#669BBC] flex flex-col h-[500px] transition-all hover:scale-105'>
         <img className='rounded h-[50%] object-cover shadow-xl' src={img} alt="" />
         <div className='p-3 flex flex-col justify-between h-[100%]'>
             <h1 className='text-[18px] font-bold'>{titulo}</h1>
             <p>{desc}</p>
-            <div className='flex gap-2 cursor-pointer'>
-              <Button className='primary text-white w-[20%]'><a target='_blank' className='text-white no-underline' href={url}>Visitar</a></Button>
+            <div className='flex gap-2 cursor-pointer items-center'>
+              <a target='_blank' className='text-white no-underline' href={url}>
+                <div className='hover:scale-105 transition-all'>
+                  <FaExternalLinkAlt size={30} color='#333'/>
+                </div>
+                </a>
               <a href={github} target='_blank'>
                 <GitHubIconDark/>
               </a>
