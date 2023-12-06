4ccfa82041bb716f2af04ec8c6adccb4510b310a
Franco Caraffa <francocaraffa2005@gmail.com>
2023-12-06 14:32:27 -0300

    v1

--- app/components/AcercaMi.jsx
+++
@@ -1,8 +0,0 @@
-// export function AcercaMi(){
-//     return (
-//       <section className='mt-32'>
-//         <h2 className='font-bold text-4xl text-[#FCDA95] mb-2'>&lt;Acerca de mi/&gt;</h2>
-//         <p className='text-[#FDF0D5] p-7 text-[18px]'>¡Hola! Soy un apasionado desarrollador frontend con una dedicación inquebrantable hacia la creación de experiencias web cautivadoras y funcionales. Mi viaje en el mundo del desarrollo web comenzó con la emoción de convertir ideas creativas en realidades digitales, y desde entonces, he estado inmerso en el emocionante universo del frontend.</p>
-//       </section>
-//     )
-// }
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -9,13 +9,14 @@ export function Header(){
     const [isOpen, setIsOpen] = useState(false)
 
     return(
-      <header className='z-10 shadow-xl bg-[#e0582ca] bg-[#003049] fixed top-0 w-[100%] '>
+      <header className='z-10 shadow-xl bg-[#e0582ca] bg-[#003049] bg-gradient-to-r from-[#003049] to-[#05121a] fixed top-0 w-[100%] '>
         <ul className='flex items-center justify-between gap-14 p-6 pl-0 pr-0 text-[#FDF0D5] h-[100%] max-w-[65%] m-auto max-[600px]:max-w-[90%]'>
           <div className="flex items-center gap-2" >
+            <li className="font-black text-xl cursor-pointer max-[600px]:">Franco</li>
             <FaCodeBranch/>
-            <li className="cursor-pointer max-[600px]:">Franco</li>
           </div>
-          <div className={`transition-all flex gap-10 max-[600px]:${isOpen ? "open" : "hidden"} max-[600px]:h-screen max-[600px]:absolute max-[600px]:bg-[rgba(17,25,40,.75)] max-[600px]:flex-col max-[600px]:justify-center max-[600px]:items-center max-[600px]:right-0 max-[600px]:left-0 max-[600px]:top-20 max-[600px]:text-2xl max-[600px]:font-bold`}>
+          <div className={`transition-all flex gap-10 max-[600px]:${isOpen ? "open" : "hidden"} max-[600px]:h-[calc(100vh-98px)] max-[600px]:absolute max-[600px]:bg-[rgba(17,25,40,.75)] max-[600px]:flex-col max-[600px]:justify-center max-[600px]:items-center max-[600px]:right-0 max-[600px]:left-0 max-[600px]:top-[98px]
+           max-[600px]:text-2xl max-[600px]:font-bold`}>
             <li onClick={()=> setIsOpen(false)}><a href="#proyectos" className="no-underline  text-white">Proyectos</a></li>
             <li onClick={()=> setIsOpen(false)}><a href="#inicio" className="no-underline text-white">Inicio</a></li>
             <li onClick={()=> setIsOpen(false)}><a href="#habilidades" className="no-underline text-white">Habilidades</a></li>
