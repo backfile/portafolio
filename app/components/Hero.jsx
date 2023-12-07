cc87b580d52b32b7a455fa009f54b40b713cbf63
Franco Caraffa <francocaraffa2005@gmail.com>
2023-12-06 22:46:21 -0300

    Mas detalles

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -6,7 +6,7 @@ import { FaAngleDoubleDown } from "react-icons/fa";
 
 export function Hero(){
     return(
-      <section id="inicio" className='flex flex-col justify-center min-h-screen mb-20'>
+      <section id="inicio" className='flex flex-col justify-center min-h-screen mb-10'>
         <div className='flex-col flex'>
           <h1 className='text-6xl font-bold text-[#FDF0D5] mb-14 max-[600px]:text-3xl max-[600px]:text-center'><span className='text-[16px] text-[#FCDA95] max-[600px]:text-xl'>¡Hola! Mi nombre es,</span><br/>Franco Caraffa.</h1>
           <TypingTitle/>
